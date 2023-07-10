39810fa8c16d12f59f501da0631be14719a4fcf7
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-07-10 23:40:09 +0530

    3 cols at sm

--- components/ImageGrid.tsx
+++ components/ImageGrid.tsx
@@ -11,7 +11,7 @@ export default function ImageGrid({
   return (
     <div className="px-6 py-4">
       <h1 className="text-xl mb-4">Today&apos;s Picks</h1>
-      <div className="xxs:columns-2 md:columns-4 2xl:columns-4 gap-4 md:gap-6">
+      <div className="xxs:columns-2 sm:columns-3 md:columns-4 2xl:columns-4 gap-4 md:gap-6">
         {imageData.map((image) => {
           return (
             <ImageCard key={image.id} image={image} selectImage={selectImage} />
